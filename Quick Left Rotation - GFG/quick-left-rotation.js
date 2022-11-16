7d358f1d445c862332a7d2bb1175ac93dc3a9ded
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-11-16 14:07:25 +0530

    Added solution - LeetHub

---
+++ Quick Left Rotation - GFG/quick-left-rotation.js
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let [n,k] = readLine().trim().split(" ").map((x) => parseInt(x));
+        let arr = readLine().trim().split(" ").map((x) => parseInt(x));
+        let obj = new Solution();
+        obj.leftRotate(arr,k,n);
+        let s = "";
+        for(let it of arr){
+            s+=it+" ";
+        }
+        console.log(s);
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} n
+*/
+
+class Solution {
+    leftRotate(arr,k,n){
+       const len = arr.length
+    k = (k % len)
+    
+    let end = arr.splice(k)
+
+    arr.splice(0,0,...end)
+       
+       
+    }
+}
