4c3a6113f631b2ec6a58e4b5f9d1de7fbe1e3345
Vaibhav Tyagi <83115094+mrtyagi07@users.noreply.github.com>
2022-11-17 10:30:04 +0530

    Added solution - LeetHub

--- Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.js
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.js
@@ -53,10 +53,29 @@ function main() {
  * @returns {number}
 */
 
+
+const binarySearch = (arr, val, start = 0, end = arr.length - 1) => {
+    const mid = Math.floor((start + end) / 2);
+
+      if (val === arr[mid]) {
+           return 1;
+         }
+
+        if (start >= end) {
+              return -1;
+           }
+
+          return val < arr[mid]? binarySearch(arr, val, start, mid - 1): binarySearch(arr, val, mid + 1, end);
+}
+
+
 class Solution {
+    
     
     searchInSorted(arr, N, K)
     {
-       return (arr.includes(K)?1:-1);
+       // return (arr.includes(K)?1:-1);
+      return binarySearch(arr,K,0,N)
+       
     }
 }
